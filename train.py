f5a2cdd1fed4e2ed9f1d865e2c1783fefd817afe
Neetha <neetha0217@gmail.com>
2026-07-10 10:11:38 +0530

    Added MLflow integration and updated sentiment analysis project

--- train.py
+++ train.py
@@ -1,3 +1,6 @@
+import mlflow
+import mlflow.pytorch
+
 import pandas as pd
 from datasets import Dataset
 from transformers import DistilBertTokenizerFast
@@ -36,7 +39,7 @@ model=DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncas
 
 training_args=TrainingArguments(
 output_dir="./results",
-evaluation_strategy="epoch",
+eval_strategy="epoch",
 per_device_train_batch_size=8,
 num_train_epochs=2
 )
@@ -47,7 +50,30 @@ args=training_args,
 train_dataset=train_dataset,
 eval_dataset=test_dataset
 )
+mlflow.set_experiment("Sentiment_Analysis_DistilBERT")
 
-trainer.train()
-model.save_pretrained("model")
-tokenizer.save_pretrained("model")
+with mlflow.start_run():
+
+    trainer.train()
+    mlflow.log_param("Model", "DistilBERT")
+    mlflow.log_param("Epochs", training_args.num_train_epochs)
+    mlflow.log_param("Batch Size", training_args.per_device_train_batch_size)
+    mlflow.log_param("Learning Rate", training_args.learning_rate)
+        
+    result = trainer.evaluate()
+
+    mlflow.log_metric("Eval Loss", result["eval_loss"])
+
+    if "eval_accuracy" in result:
+        mlflow.log_metric("Accuracy", result["eval_accuracy"])
+        
+        mlflow.pytorch.log_model(model, "model")
+        mlflow.pytorch.log_model(tokenizer, "tokenizer")
+        
+        trainer.save_model("model")
+
+        mlflow.pytorch.log_model(model, "SentimentModel")
+
+        mlflow.log_artifact("reviews.csv")
+        mlflow.log_artifact("Dockerfile")
+        mlflow.log_artifact("model_card.md")
